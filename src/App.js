cfb7dff810f1c84ba4f3d835019a28565acf5bf5
hyunjong_96 <ahajongs@gmail.com>
2021-01-30 14:26:41 +0900

    #5 subRoute

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Link} from 'react-router-dom'
 import About from './About'
 import Home from './Home'
 import Profile from './Profile'
+import Profiles from './Profiles'
 
 function App() {
   return (
@@ -14,11 +15,14 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profiles">프로필 목록</Link>
+        </li>
       </ul>
       <hr/>
     <Route path="/" exact={true} component={Home}/>
-    <Route path="/about" component={About}/>
-    <Route path="/profiles/:username" component={Profile}/>
+    <Route path="/about" exact={true} component={About}/>
+    <Route path="/profiles" component={Profiles}/>
     </div>
   );
 }
